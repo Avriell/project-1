1e775f88ac39ad6bcc07ea7fcbf405a8a07f15fe
avriel <ceuoss@gmail.com>
2022-11-03 23:26:07 -0400

    working nav bar

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 $(() => {
+//   $('#home').hide()
+  $('#bio').hide()
+  $('#projects').hide()
+  $('#resume').hide()
+
+  const homeShow = () => {
+    $('#home').show()
+    $('#bio').hide()
+    $('#projects').hide()
+    $('#resume').hide()
+  }
+  $('#hButton').on('click', homeShow)
+
+  const bioShow = () => {
+    // $('#home').hide()
+    $('#bio').show()
+    $('#projects').hide()
+    $('#resume').hide()
+  }
+    $('#bButton').on('click', bioShow)
+
+    
     $('.change').on('click', function() {
         if( $('body').hasClass('dark')) {
             $('body').removeClass('dark');
-            // $( ".change" ).text( "OFF" );
         } else {
             $('body').addClass('dark');
-            // $( ".change" ).text( "ON" );
         }
  });
-    
+    $('.hide').on('click', hide())
 })

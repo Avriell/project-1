c711c9c300f34ece73866a586deea054f9570f79
avriel <ceuoss@gmail.com>
2022-11-06 03:49:10 -0500

    everything working need projects and resume

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ $(() => {
   $('#bio').hide()
   $('#projects').hide()
   $('#resume').hide()
+  $('#hobbies').hide()
 
   const homeShow = () => {
     $('#home').show()
     $('#bio').hide()
     $('#projects').hide()
     $('#resume').hide()
+    $('#hobbies').hide()
   }
   $('#hButton').on('click', homeShow)
 
@@ -17,10 +19,20 @@ $(() => {
     $('#bio').show()
     $('#projects').hide()
     $('#resume').hide()
+    $('#hobbies').hide()
   }
     $('#bButton').on('click', bioShow)
 
-    
+    const hobShow = () => {
+      // $('#home').hide()
+      $('#bio').hide()
+      $('#projects').hide()
+      $('#resume').hide()
+      $('#hobbies').show()
+    }
+      $('#hobButton').on('click', hobShow)
+
+
     $('.change').on('click', function() {
         if( $('body').hasClass('dark')) {
             $('body').removeClass('dark');

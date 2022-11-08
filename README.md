8efd896b6f5e4c7baa20b8e73c14abfea085e667
avriel <ceuoss@gmail.com>
2022-11-07 19:51:51 -0500

    Slightly finished but finished project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# project-1
-repo for first project
+The three JS/JQuery interactions I used is 
+1. When clicking "bio" .show() bio and .hide() the rest
+2. Dark mode was made using .on'click' addClass of dark if it already has it then remove class of dark (dark class has css to change background color and text color)
+3. Gave "Don't click me" a class of .hidden and when clicked, triggers a function in JQuery that prepends the rat fighting gif to .hidden
+4. I also followed a youtube video to make a slider with two images that slides using radio buttons and them receiving input.
+
+Approach taken, at first I was caught up on trying to figure out the CSS and how to get everything put in the right place, then instead focused on getting all the functionalities working and working on css towards the end. My css is definitely lacking and I am not a fan but hope to improve on it still.
+
+Unsolved problems include figuring out how to shrink my slider to fit without breaking so I can use it in a media query to fit everything so that it is responsive. Also moving things around with css so that things are on the page where I want it to be.
--- app.js
+++ app.js
@@ -7,46 +7,47 @@ $(() => {
 
   const homeShow = () => {
     $('#home').show()
-    $('#bio').hide()
-    $('#projects').hide()
-    $('#resume').hide()
-    $('#hobbies').hide()
+    $('#bio').hide(1000)
+    $('#projects').hide(1000)
+    $('#resume').hide(1000)
+    $('#hobbies').hide(1000)
   }
   $('#hButton').on('click', homeShow)
 
   const bioShow = () => {
     // $('#home').hide()
-    $('#bio').show()
-    $('#projects').hide()
-    $('#resume').hide()
-    $('#hobbies').hide()
+    $('#bio').show(400)
+    $('#projects').hide(1000)
+    $('#resume').hide(1000)
+    $('#hobbies').hide(1000)
   }
     $('#bButton').on('click', bioShow)
 
     const hobShow = () => {
       // $('#home').hide()
-      $('#bio').hide()
-      $('#projects').hide()
-      $('#resume').hide()
-      $('#hobbies').show()
+      $('#bio').hide(1000)
+      $('#projects').hide(1000)
+      $('#resume').hide(1000)
+      $('#hobbies').show(400)
+      
     }
       $('#hobButton').on('click', hobShow)
 
       const resumeShow = () => {
         // $('#home').hide()
-        $('#bio').hide()
-        $('#projects').hide()
-        $('#resume').show()
-        $('#hobbies').hide()
+        $('#bio').hide(1000)
+        $('#projects').hide(1000)
+        $('#resume').show(400)
+        $('#hobbies').hide(1000)
       }
         $('#rButton').on('click', resumeShow)
 
         const projectsShow = () => {
           // $('#home').hide()
-          $('#bio').hide()
-          $('#projects').show()
-          $('#resume').hide()
-          $('#hobbies').hide()
+          $('#bio').hide(1000)
+          $('#projects').show(400)
+          $('#resume').hide(1000)
+          $('#hobbies').hide(1000)
         }
           $('#pButton').on('click', projectsShow)
 
@@ -58,6 +59,17 @@ $(() => {
             $('body').addClass('dark');
         }
  });
+
+    
+    $('.hidden').click(
+      function () {
+        $('.hidden').prepend('<img src="rat-fight.gif" class="remove"/> <p <a href="#" class="remove">REMOVE ME!</a></p>').delay(6000).hide(500)
+      }
+    )
+    // $('.remove').on('click', function() {
+    //   $('.remove').remove()
+    // })
+    
 
     // const dropDown = () => {
     //   $('.menu').toggle(function() {
